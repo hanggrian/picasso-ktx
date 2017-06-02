c70271ce807f33c14af01b24ac95f25d951eaaf8
Hendra Anggrian <hendraanggrian@gmail.com>
2017-06-03 04:00:10 +0700

    0.11.6

--- picasso-commons/src/main/java/com/hendraanggrian/picasso/commons/target/PlaceholderTargeter.java
+++ picasso-commons/src/main/java/com/hendraanggrian/picasso/commons/target/PlaceholderTargeter.java
@@ -5,14 +5,12 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.hendraanggrian.support.utils.view.ViewGroups;
-import com.hendraanggrian.support.utils.view.Views;
 import com.squareup.picasso.Picasso;
 
 /**
@@ -24,24 +22,16 @@ class PlaceholderTargeter extends Targeter {
 
     @NonNull private final ViewGroup parent;
     @NonNull private final ImageView target;
-    private int targetPosition;
-    @NonNull private final ViewGroup placeholderContainer;
-    @Nullable private ImageView placeholder;
+    @NonNull private final ViewGroup placeholder;
 
-    PlaceholderTargeter(@NonNull ImageView target, @NonNull View placeholder) {
+    PlaceholderTargeter(@NonNull ImageView target, @NonNull View placeholderView) {
         this.parent = (ViewGroup) target.getParent();
         this.target = target;
         this.target.setTag(this);
-        for (int i = 0; i < parent.getChildCount(); i++) {
-            if (parent.getChildAt(i) == target) {
-                targetPosition = i;
-                break;
-            }
-        }
-        this.placeholderContainer = new FrameLayout(target.getContext());
-        this.placeholderContainer.setLayoutParams(target.getLayoutParams());
-        this.placeholderContainer.addView(placeholder);
-        this.placeholderContainer.setTag(TAG);
+        this.placeholder = new FrameLayout(target.getContext());
+        this.placeholder.setLayoutParams(target.getLayoutParams());
+        this.placeholder.addView(placeholderView);
+        this.placeholder.setTag(TAG);
         transition(true);
     }
 
@@ -57,11 +47,7 @@ class PlaceholderTargeter extends Targeter {
 
     @Override
     public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-        if (placeholder != null) {
-            ViewGroups.removeViews(placeholderContainer, placeholder);
-            placeholder = null;
-        }
-        removePlaceholders();
+        ViewGroups.removeViews(parent, ViewGroups.findViewsWithTag(parent, TAG, false));
         target.setVisibility(View.VISIBLE);
         target.setImageBitmap(bitmap);
         super.onBitmapLoaded(bitmap, from);
@@ -69,11 +55,7 @@ class PlaceholderTargeter extends Targeter {
 
     @Override
     public void onBitmapFailed(Drawable errorDrawable) {
-        if (placeholder != null) {
-            ViewGroups.removeViews(placeholderContainer, placeholder);
-            placeholder = null;
-        }
-        removePlaceholders();
+        ViewGroups.removeViews(parent, ViewGroups.findViewsWithTag(parent, TAG, false));
         target.setVisibility(View.VISIBLE);
         target.setImageDrawable(errorDrawable);
         super.onBitmapFailed(errorDrawable);
@@ -82,35 +64,25 @@ class PlaceholderTargeter extends Targeter {
     @Override
     public final void onPrepareLoad(Drawable placeHolderDrawable) {
         if (placeHolderDrawable != null) {
-            placeholder = new ImageView(target.getContext());
-            placeholder.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-            placeholder.setImageDrawable(placeHolderDrawable);
-            placeholderContainer.addView(placeholder, 0);
+            ImageView placeholderView = new ImageView(target.getContext());
+            placeholderView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            placeholderView.setImageDrawable(placeHolderDrawable);
+            placeholder.addView(placeholderView, 0);
         }
-        removePlaceholders();
-        parent.addView(placeholderContainer, targetPosition);
+        ViewGroups.removeViews(parent, ViewGroups.findViewsWithTag(parent, TAG, false));
+        parent.addView(placeholder, parent.indexOfChild(target));
         target.setVisibility(View.GONE);
         super.onPrepareLoad(placeHolderDrawable);
     }
 
     @NonNull
     public PlaceholderTargeter transition(boolean enable) {
-        ViewGroup parent = Views.getParent(target);
+        ViewGroup parent = (ViewGroup) target.getParent();
         if (parent != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
             if (enable && parent.getLayoutTransition() == null)
                 parent.setLayoutTransition(new LayoutTransition());
             else if (!enable)
                 parent.setLayoutTransition(null);
         return this;
-    }
-
-    private void removePlaceholders() {
-        for (int i = 0; i < parent.getChildCount(); i++) {
-            View child = parent.getChildAt(i);
-            Object tag = child.getTag();
-            if (tag != null && tag.equals(TAG)) {
-                parent.removeView(child);
-            }
-        }
     }
 }
